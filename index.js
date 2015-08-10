79d7712b74216ff3b0309996e2da16ce7b942547
danieljoppi <daniel.joppi@gmail.com>
2015-08-10 15:28:40 -0300

    update jsdom for proxy release 1.2.0

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ function getProfile(linkedInURL, callback) {
     jsdom.env({
         url: linkedInURL,
         scripts: ["http://code.jquery.com/jquery.js"],
+        headers: {
+            'Accept':'text/html',
+            //'Accept-Encoding': 'gzip',
+            'Accept-Language': 'en-US;q=0.6,en;q=0.4',
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36'
+        },
+        proxy: process.env.PROXY_HOST,
+        tunnel: process.env.PROXY_HOST ? false : undefined,
         done: function (errors, window) {
+            console.log(errors);
+            if (errors) return callback(new ProfileClass());
             var $ = window.$;
+            console.log($('html').html());
             var profile = new ProfileClass();
 
             profile.publicProfileUrl = linkedInURL;
