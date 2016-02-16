52a8600f873bcb67231c7febf204d920d12e0d56
daniel.joppi <daniel.joppi@gmail.com>
2016-02-16 12:00:26 -0200

    remove travis ignore

--- test/scraper-test.js
+++ test/scraper-test.js
@@ -9,20 +9,18 @@ describe('<LinkedIn Scraper>', function() {
 
     function validateProfile(profile) {
         should(profile).be.ok();
-        if (!process.env.TRAVIS) {
-            should(profile).have.property('name', 'Daniel Henrique Joppi');
-            should(profile).have.property('headline', 'Software Architect na Rospo GeoTech');
-            should(profile).have.property('location', 'Florianópolis, Santa Catarina, Brazil');
-            should(profile).have.property('summary').be.ok();
-            should(profile).have.property('languages').length(5);
-            should(profile).have.property('skills').length(45);
-            should(profile).have.property('currentPositions').length(3);
-            should(profile).have.property('pastPositions').length(3);
-            should(profile).have.property('educations').length(1);
-            should(profile).have.property('positions').length(8);
-            //should(profile).have.property('honors').length(0);
-            //should(profile).have.property('projects').length(0);
-        }
+        should(profile).have.property('name', 'Daniel Henrique Joppi');
+        should(profile).have.property('headline', 'Software Architect na Rospo GeoTech');
+        should(profile).have.property('location', 'Florianópolis, Santa Catarina, Brazil');
+        should(profile).have.property('summary').be.ok();
+        should(profile).have.property('languages').length(5);
+        should(profile).have.property('skills').length(45);
+        should(profile).have.property('currentPositions').length(3);
+        should(profile).have.property('pastPositions').length(3);
+        should(profile).have.property('educations').length(1);
+        should(profile).have.property('positions').length(8);
+        //should(profile).have.property('honors').length(0);
+        //should(profile).have.property('projects').length(0);
         should(profile).have.property('publicProfileUrl', url);
     }
 
